45584d51132460db778c5f6905d6b84e25724992
Ubuntu <ubuntu@ip-172-31-24-113.us-east-2.compute.internal>
2018-06-03 06:52:51 +0000

    got most of the charts in

--- all_functions.R
+++ all_functions.R
@@ -70,26 +70,123 @@ expirationDate <-function(ticker, o_type) {
   return(xDate)
 } 
 
-#this function will generate the first chart of volume vs price 
+#this function will generate the first chart of volume vs strike price 
 chartVolumePrice <-function(xFrame, xDate) {
   c<-subset(xFrame, xFrame$ExpirationDate == xDate)
+ 
+  
   highchart() %>%
-  hc_add_series(c, hcaes(x=Strike, y=Vol, z=VolumeWeight), type = "column",
+    hc_add_series(c, hcaes(x=Strike, y=Vol), type = "column",
                   name = "Volume Traded",
-                  color = 'blue', showInLegend = FALSE) %>%
-  hc_yAxis(title = list(text = "Volume Traded"), allowDecimals = FALSE) %>%
-  hc_xAxis(data =c$Strike,
+                  color = '#4682B4', showInLegend = FALSE) %>%
+    hc_yAxis(title = list(text = "Volume Traded"), allowDecimals = FALSE, showLastLabel = FALSE) %>%
+    
+    hc_xAxis(data =c$Strike,
              tickmarkPlacement = "on",
              opposite = FALSE) %>%
-  hc_title(text = '2018-06-08',
+    hc_title(text = xDate,
              style = list(fontWeight = "bold")) %>%
-  hc_subtitle(text = "Volume Traded Vs Strike Price") %>%
-  hc_tooltip(valueDecimals = 0,
-               pointFormat = "Percent: {point.z:.2f} <br> Volume: {point.y}") %>%
-  hc_add_theme(hc_theme_538())
+    hc_subtitle(text = "Volume Traded Vs Strike Price") %>%
+    hc_tooltip(valueDecimals = 0,
+               pointFormat = "{series.name} {point.y:.2f}") %>%
+    hc_add_theme(hc_theme_538())
+}
+
+#this function will generate the IV vs Strike
+chartIVStrike <-function(xFrame, xDate) {
+    c<-subset(xFrame, xFrame$ExpirationDate == xDate)
+    highchart() %>%
+      hc_xAxis(c, hcaes(x=Strike), name = "Strike Price")%>% 
+      hc_yAxis(title = list(text = "Daily Implied Volatility"),
+                              labels=list(format = '{value}%'), 
+               showFirstLabel = TRUE,showLastLabel=TRUE,opposite = FALSE) %>%
+      
+      hc_add_series(c, hcaes(x=Strike, y=(ImpliedVol/(252^.5))*100), type = "column",
+                    name = "Daily Implied Volatility",
+                    color = '#4682B4', showInLegend = FALSE) %>%
+        
+      hc_add_series(c, hcaes(x=Strike, y=(U_Vol45days/(252^.5))*100), type = "line",
+                    name = "Last 45 days historical volatility",
+                    color = '#ffff00', showInLegend = TRUE) %>%
+
+      hc_add_series(c, hcaes(x=Strike, y=(U_Vol1year/(252^.5))*100), type = "line",
+                    name = "Last trailng year historical volatility",
+                    color = '#006400', showInLegend = TRUE) %>%
+      
+      hc_tooltip(valueDecimals = 0,
+                 pointFormat = "{series.name} {point.y:.2f}%") %>%
+      hc_title(text = xDate,
+               style = list(fontWeight = "bold")) %>%
+      hc_subtitle(text = "Strike Price vs IV") %>%
+      hc_add_theme(hc_theme_538())
+    
 }
 
 
+chartInMoneyStrike <-function(xFrame, xDate) {
+  c<-subset(xFrame, xFrame$ExpirationDate == xDate)
+  highchart() %>%
+    hc_xAxis(c, hcaes(x=Strike), name = "Strike Price")%>% 
+    hc_yAxis(title = list(text = "Probability of breaking even"),
+             labels=list(format = '{value}%'), 
+             showFirstLabel = TRUE,showLastLabel=TRUE,opposite = FALSE) %>%
+    
+    hc_add_series(c, hcaes(x=Strike, y=BreakEvenProbability*100), type = "spline",
+                  name = "Probablility of breaking even",
+                  color = '#4682B4', showInLegend = FALSE) %>%
+    
+    hc_tooltip(valueDecimals = 0,
+               pointFormat = "{series.name} {point.y:.2f}%") %>%
+    hc_title(text = xDate,
+             style = list(fontWeight = "bold")) %>%
+    hc_subtitle(text = "Probability of breaking even at different Strike") %>%
+    hc_add_theme(hc_theme_538())
+  
+}
+#testing scripts
+# xDate <- '2018-06-08'
+# c<-optionWeights('AAPL', 'c')
+# c<-subset(c, c$ExpirationDate == xDate)
+# c$dailyImpliedVol<-c$ImpliedVol/(252^.5)
+# c$daily_U_MeanDailyReturn_1year <-c$U_Vol1year/(252^.5)
+# c$daily_U_Mean45 <-c$U_Vol45days/(252^.5)
+# max(c$dailyImpliedVol)
+# max(c$daily_U_Mean45)
 
+
+
+#c<-chartVolumePrice (c, '2018-06-08')
+
+# highchart() %>% 
+#   hc_add_series(data = c(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2,
+#                          26.5, 23.3, 18.3, 13.9, 9.6),
+#                 type = "spline") %>% 
+#   hc_xAxis(title = list(text = "x Axis at top"),
+#            opposite = TRUE,
+#            plotLines = list(
+#              list(label = list(text = "This is a plotLine"),
+#                   color = "#'FF0000",
+#                   width = 2,
+#                   value = 5.5))) %>% 
+#   hc_yAxis(title = list(text = "y Axis at right"),
+#            opposite = TRUE,
+#            minorTickInterval = "auto",
+#            minorGridLineDashStyle = "LongDashDotDot",
+#            showFirstLabel = FALSE,
+#            showLastLabel = FALSE,
+#            plotBands = list(
+#              list(from = 25, to = 80, color = "rgba(100, 0, 0, 0.1)",
+#                   label = list(text = "This is a plotBand")))) 
+# 
+# highchart() %>% 
+#   hc_yAxis_multiples(
+#     list(top = "0%", height = "30%", lineWidth = 3),
+#     list(top = "30%", height = "70%", offset = 0,
+#          showFirstLabel = FALSE, showLastLabel = FALSE)
+#   ) %>% 
+#   hc_add_series(data = rnorm(10)) %>% 
+#   hc_add_series(data = rexp(10), type = "spline", yAxis = 1)
+
+  
 
 
